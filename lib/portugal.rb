6f7abdb96fa5f738d4ac3ee800115ab6ac209095
Rémi Prévost <remi@exomel.com>
2013-02-26 11:29:52 -0500

    Move code to the Portugal module

--- lib/portugal.rb
+++ lib/portugal.rb
@@ -9,4 +9,44 @@ module Portugal
   def self.config
     @config
   end
+
+  class Migrator
+    def self.migrate!
+      ActiveRecord::Migration.verbose = true
+      ActiveRecord::Migrator.migrate(Portugal.config.migrations_path)
+    end
+
+    def self.rollback!(step)
+      ActiveRecord::Migration.verbose = true
+      ActiveRecord::Migrator.rollback(Portugal.config.migrations_path, step)
+    end
+  end
+
+  class Migration
+    def initialize(name)
+      migration_number = Dir[File.join(Portugal.config.migrations_path, '*.rb')].inject(0) do |memo, item|
+        filename = File.basename(item, File.extname(item))
+        number = filename.gsub(/\A(\d+)_.*\Z/, '\1').to_i + 1
+        number > memo ? number : memo
+      end
+
+      content = <<CONTENT
+class #{name.camelize} < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+
+  # def change
+  # end
+end
+CONTENT
+
+      migration_file = File.join(Portugal.config.migrations_path, "#{"%03d" % migration_number}_#{name.underscore}.rb")
+      File.open(migration_file, 'w+') { |data| data << content }
+
+      puts migration_file
+    end
+  end
 end
--- lib/portugal/tasks.rb
+++ lib/portugal/tasks.rb
@@ -3,44 +3,18 @@ require 'portugal'
 namespace :db do
   desc "Migrate the database"
   task :migrate => :environment do
-    ActiveRecord::Migration.verbose = true
-    ActiveRecord::Migrator.migrate(Portugal.config.migrations_path)
+    Portugal::Migrator.migrate!
   end
 
   desc "Rolls the database back to the previous version (specify steps w/ STEP=n)."
   task :rollback => :environment do
     step = ENV['STEP'] ? ENV['STEP'].to_i : 1
-    ActiveRecord::Migration.verbose = true
-    ActiveRecord::Migrator.rollback(Portugal.config.migrations_path, step)
+    Portugal::Migrator.rollback!(step)
   end
 
   desc "Create a new migration file (specify name w/ NAME=do_something)"
   task :new_migration => :environment do
-    migration_name = ENV["NAME"]
-
-    migration_number = Dir[File.join(Portugal.config.migrations_path, '*.rb')].inject(0) do |memo, item|
-      filename = File.basename(item, File.extname(item))
-      number = filename.gsub(/\A(\d+)_.*\Z/, '\1').to_i + 1
-      number > memo ? number : memo
-    end
-
-    content = <<CONTENT
-class #{migration_name.camelize} < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-
-  # def change
-  # end
-end
-CONTENT
-
-    migration_file = File.join(Portugal.config.migrations_path, "#{"%03d" % migration_number}_#{migration_name.underscore}.rb")
-    File.open(migration_file, 'w+') { |data| data << content }
-
-    puts migration_file
+    Portugal::Migration.new(ENV["NAME"])
   end
 end
 
